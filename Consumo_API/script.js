63d9e8056673c5b9a57091ca55b8610994b13b0c
MarineVega <marinev33@hotmail.com>
2024-09-16 15:18:53 -0300

    Renombré archivo js, agregué estilos

---
+++ Consumo_API/script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+let contenedor = document.getElementById("contenedor")
+/*
+fetch("https://rickandmortyapi.com/api/character")
+.then(resp => resp.json())
+.then(data =>{
+    data.results.forEach(personaje => {
+        contenedor.innerHTML +=`<h2>${personaje.name} </h2>
+                                <img src=${personaje.image}>
+                                <h4>Género: ${personaje.gender}</h4>
+                                <h4>Especie: ${personaje.species}</h4>`
+    });
+
+})
+*/
+
+let url = "https://rickandmortyapi.com/api/character"
+
+async function mostrarPersonajes() {
+    let respuesta = await fetch(url)
+    let data = await respuesta.json()
+
+    data.results.forEach(personaje => {
+        contenedor.innerHTML +=`<div class="cardPersonaje">
+                                    <h2>${personaje.name} </h2>
+                                    <img src=${personaje.image}>
+                                    <h4>Género: ${personaje.gender}</h4>
+                                    <h4>Especie: ${personaje.species}</h4>
+                                </div>`
+    });  
+}
+
+mostrarPersonajes();
